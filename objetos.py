e9c25560a64d5e55d7590404eecf27be53fd4858
Gaston Gomez <gaston.gomez.1998@gmail.com>
2023-06-07 10:05:46 -0300

    Los invasores disparan misiles y la nave recibe disparos

--- objetos.py
+++ objetos.py
@@ -1,6 +1,19 @@
 import pygame
 import random
 
+class Misil():
+    def __init__(self, x, y, ancho, alto, color) -> None:
+        self.imagen = pygame.image.load('Parcial_2/img/misil.png')
+        self.ancho = ancho
+        self.alto = alto
+        self.imagen = pygame.transform.scale(self.imagen, (ancho, alto))
+        self.imagen = pygame.transform.rotate(self.imagen, 180)
+        self.rect = self.imagen.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.color = color
+        self.disparo = False
+        self.velocidad = 10
 
 
 class Nave():
@@ -13,13 +26,34 @@ class Nave():
         self.rect.x = x
         self.rect.y = y
         self.color = color
+        self.misil = Misil(self.rect.x+self.ancho/2-5,700,10,30,(0,0,255))
+        self.frente = pygame.Rect(self.rect.x+self.ancho/2-15, self.rect.y, 30, 10)
+        self.alas = pygame.Rect(self.rect.x+self.ancho/2-25, self.rect.y+30, 50, 10)
     
     def mover_nave(self,pos):
         if pos < 550:
             self.rect.x = pos
+            self.frente.x = self.rect.x+self.ancho/2-15
+            self.alas.x = self.rect.x+self.ancho/2-25
+            if not self.misil.disparo:
+                self.misil.rect.x = self.rect.x+self.ancho/2-5
+    
+    def disparar_misil(self):
+        if self.misil.disparo:
+            self.misil.rect.y -= self.misil.velocidad
+            if self.misil.rect.y <= self.misil.alto-self.misil.alto*2:
+                self.misil.rect.y = self.rect.y
+                self.misil.rect.x = self.rect.x+self.ancho/2-self.misil.ancho/2
+                self.misil.disparo = False
+    
+    def recibir_disparo(self, invasor):
+        if self.frente.colliderect(invasor.misil.rect) or self.alas.colliderect(invasor.misil.rect):
+            print("Un misil le dio a la nave")
 
-class Invasor():
-    def __init__(self, ancho, alto, color) -> None:
+
+class Invasor(pygame.sprite.Sprite):
+    def __init__(self, ancho, alto, color):
+        super().__init__()
         self.imagen = pygame.image.load('Parcial_2/img/invasor.png')
         self.ancho = ancho
         self.alto = alto
@@ -27,52 +61,45 @@ class Invasor():
         self.rect = self.imagen.get_rect()
         self.rect.x = random.randrange(0, 600-ancho)
         self.rect.y = random.randrange(0, 500-alto)
-        self.velocidad_x = random.randrange(2,5)
-        self.velocidad_y = random.randrange(2,5)
+        self.velocidad_x = random.randrange(1,7)
+        self.velocidad_y = random.randrange(1,7)
         self.color = color
         self.punto_disparo = pygame.Rect(self.rect.x+10, self.rect.y+50-10, 50, 10)
+        self.misil = Misil(self.rect.x+self.ancho/2, self.rect.y, 10,30,(0,0,255))
     
-    def recibir_disparo(self, misil, nave: Nave, timer, evento):
-        if evento.type == timer and misil.disparo:
-            if self.punto_disparo.colliderect(misil.rect):
-                self.rect.x = random.randrange(0, 600-self.ancho)
-                self.rect.y = random.randrange(0, 500/2-self.alto)
-                self.punto_disparo = pygame.Rect(self.rect.x+10, self.rect.y+50-10, 50, 10)
-                misil.rect.y = nave.rect.y
-                misil.rect.x = nave.rect.x+nave.ancho/2-misil.ancho/2
-                misil.disparo = False
+    def recibir_disparo(self, nave: Nave, puntos: int):
+        if self.punto_disparo.colliderect(nave.misil.rect):
+            self.rect.x = random.randrange(0, 600-self.ancho)
+            self.rect.y = random.randrange(0, 500/2-self.alto)
+            self.punto_disparo = pygame.Rect(self.rect.x+10, self.rect.y+50-10, 50, 10)
+            nave.misil.rect.y = nave.rect.y
+            nave.misil.rect.x = nave.rect.x+nave.ancho/2-nave.misil.ancho/2
+            nave.misil.disparo = False
+            puntos = puntos + 1
+
+        return puntos
+            
     
-    def mover(self):
+    def mover(self, alto_pantalla, ancho_pantalla):
         self.rect.x += self.velocidad_x
         self.rect.y += self.velocidad_y
         self.punto_disparo = pygame.Rect(self.rect.x+10, self.rect.y+50-10, 50, 10)
-
+        if not self.misil.disparo:
+            self.misil.rect.y = self.rect.y
+            self.misil.rect.x = self.rect.x+self.ancho/2-5 
         if self.rect.left < 0:
-            self.velocidad_x += 2
-        if self.rect.right > 600:
-            self.velocidad_x -= 2
-        if self.rect.bottom > 500:
-            self.velocidad_y -= 2
+            self.velocidad_x += 1
+        if self.rect.right > ancho_pantalla:
+            self.velocidad_x *= -1
+        if self.rect.bottom > alto_pantalla*0.6:
+            self.velocidad_y *= -1
         if self.rect.top < 0:
-            self.velocidad_y += 2
-
-
-class Misil():
-    def __init__(self, x, y, ancho, alto, color) -> None:
-        self.imagen = pygame.image.load('Parcial_2/img/misil.png')
-        self.ancho = ancho
-        self.alto = alto
-        self.imagen = pygame.transform.scale(self.imagen, (ancho, alto))
-        self.rect = self.imagen.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.color = color
-        self.disparo = False
+            self.velocidad_y += 1
     
-    def disparar(self, nave: Nave, evento, timer):
-        if evento.type == timer and self.disparo:
-            self.rect.y -= 10
-            if self.rect.y <= self.alto-self.alto*2:
-                self.rect.y = nave.rect.y
-                self.rect.x = nave.rect.x+nave.ancho/2-self.ancho/2
-                self.disparo = False
+    def disparar_misil(self, alto_pantalla):
+        self.misil.disparo = True
+        self.misil.rect.y += random.randrange(3,5)
+        if self.misil.rect.y >= alto_pantalla + alto_pantalla/2:
+            self.misil.rect.y = self.rect.y
+            self.misil.rect.x = self.rect.x+self.ancho/2-self.misil.ancho/2
+            self.misil.disparo = False
--- parcial2.py
+++ parcial2.py
@@ -3,28 +3,36 @@ import random
 import sys
 import objetos
 
-def crear_invasores(cantidad):
+def crear_invasores(cantidad, lista_invasores):
 
     for _ in range(cantidad):
         invasor = objetos.Invasor(70,50,(0,255,0))
-        invasores.append(invasor)
-
-aux_nave = objetos.Nave(350,700,50,50,(255,0,0))
-aux_misil = objetos.Misil(aux_nave.rect.x+aux_nave.ancho/2-5,700,10,30,(0,0,255))
-fondo_imagen = pygame.image.load("Parcial_2/img/fondo.png")
-invasores = []
-crear_invasores(5)
+        lista_invasores.add(invasor)
 
 
 pygame.init()
 
+fondo_imagen = pygame.image.load("Parcial_2/img/fondo.png")
+ALTO_PANTALLA = 800
+ANCHO_PANTALLA = 600
+
+nave = objetos.Nave(350,700,50,50,(255,0,0))
+invasores = pygame.sprite.Group()
+puntaje = 0
+fuente_puntaje = pygame.font.SysFont('Helvetica', 30)
+
+crear_invasores(5, invasores)
+
 running = True
 
-window = pygame.display.set_mode((600,800))
-pygame.display.set_caption("Juego")
+timer_10_milisegundos = pygame.USEREVENT
+pygame.time.set_timer(timer_10_milisegundos, 10)
 
-timer_disparo_misil = pygame.USEREVENT
-pygame.time.set_timer(timer_disparo_misil, 10)
+timer_segundo = pygame.USEREVENT+1
+pygame.time.set_timer(timer_segundo, 1000)
+
+window = pygame.display.set_mode((ANCHO_PANTALLA,ALTO_PANTALLA))
+pygame.display.set_caption("Juego")
 
 while running:
 
@@ -35,25 +43,37 @@ while running:
             running = False
             sys.exit()
         if evento.type == pygame.MOUSEMOTION:
-            aux_nave.mover_nave(evento.pos[0])
-            if not aux_misil.disparo:
-                aux_misil.rect.x = aux_nave.rect.x+aux_nave.ancho/2-5
+            nave.mover_nave(evento.pos[0])
+
         if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
-            aux_misil.disparo = True
-        aux_misil.disparar(aux_nave, evento, timer_disparo_misil)
-        for invasor in invasores:
-            invasor.recibir_disparo(aux_misil, aux_nave, timer_disparo_misil, evento)
-            invasor.mover()
+            nave.misil.disparo = True
+            
+        if evento.type == timer_10_milisegundos:
+            nave.disparar_misil()
+            for invasor in invasores:
+                invasor.mover(ALTO_PANTALLA, ANCHO_PANTALLA)
+                invasor.disparar_misil(ALTO_PANTALLA)
+            
+        
+    for invasor in invasores:
+        puntaje = invasor.recibir_disparo(nave, puntaje)
+        puntaje_render = fuente_puntaje.render(f"Puntaje: {puntaje}", True, (255,255,255))
+        nave.recibir_disparo(invasor)
     
     window.fill((0,0,0))
     window.blit(fondo_imagen, (0,0))
-    #pygame.draw.rect(window, aux_nave.color, aux_nave.rect)
-    #pygame.draw.rect(window, aux_misil.color, aux_misil.rect)
-    window.blit(aux_misil.imagen, aux_misil.rect)
-    window.blit(aux_nave.imagen, aux_nave.rect)
+    #pygame.draw.rect(window, nave.color, nave.rect)
+    window.blit(nave.imagen, nave.rect)
+    #pygame.draw.rect(window, nave.misil.color, nave.misil.rect)
+    window.blit(nave.misil.imagen, nave.misil.rect)
     for invasor in invasores:
         #pygame.draw.rect(window, invasor.color, invasor.rect)
         #pygame.draw.rect(window, (100,100,100), invasor.punto_disparo)
+        #pygame.draw.rect(window, invasor.misil.color, invasor.misil.rect)
+        window.blit(invasor.misil.imagen, invasor.misil.rect)
         window.blit(invasor.imagen, invasor.rect)
+    window.blit(puntaje_render, (5,20))
+    #pygame.draw.rect(window, (0,0,0), nave.frente)
+    #pygame.draw.rect(window, (0,0,0), nave.alas)
     pygame.display.flip()
     pygame.display.update()
