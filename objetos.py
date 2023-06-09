701a05bf8c672e95c395bfea2381fddd2f5803ea
Gaston Gomez <gaston.gomez.1998@gmail.com>
2023-06-09 11:28:24 -0300

    Se agregaron sonidos de disparos

--- objetos.py
+++ objetos.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import pygame.mixer
 
 class Misil():
     def __init__(self, x, y, ancho, alto, color) -> None:
@@ -7,13 +8,11 @@ class Misil():
         self.ancho = ancho
         self.alto = alto
         self.imagen = pygame.transform.scale(self.imagen, (ancho, alto))
-        self.imagen = pygame.transform.rotate(self.imagen, 180)
         self.rect = self.imagen.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.color = color
         self.disparo = False
-        self.velocidad = 10
 
 
 class Nave():
@@ -29,6 +28,15 @@ class Nave():
         self.misil = Misil(self.rect.x+self.ancho/2-5,700,10,30,(0,0,255))
         self.frente = pygame.Rect(self.rect.x+self.ancho/2-15, self.rect.y, 30, 10)
         self.alas = pygame.Rect(self.rect.x+self.ancho/2-25, self.rect.y+30, 50, 10)
+        self.vidas = 3
+        self.destruida = False
+        self.invasores_destruidos = 0
+        self.recuperandose = False
+        self.sonido_disparo = pygame.mixer.Sound("Parcial_2/sound/laser1.wav")
+        self.sonido_disparo.set_volume(0.5)
+        self.sonido_destruido = pygame.mixer.Sound("Parcial_2/sound/explosion_nave.mp3")
+        self.sonido_destruido.set_volume(0.5)
+        self.ejecutar_sonido = True
     
     def mover_nave(self,pos):
         if pos < 550:
@@ -40,19 +48,23 @@ class Nave():
     
     def disparar_misil(self):
         if self.misil.disparo:
-            self.misil.rect.y -= self.misil.velocidad
+            self.misil.rect.y -= 10
             if self.misil.rect.y <= self.misil.alto-self.misil.alto*2:
                 self.misil.rect.y = self.rect.y
                 self.misil.rect.x = self.rect.x+self.ancho/2-self.misil.ancho/2
                 self.misil.disparo = False
     
     def recibir_disparo(self, invasor):
-        if self.frente.colliderect(invasor.misil.rect) or self.alas.colliderect(invasor.misil.rect):
-            print("Un misil le dio a la nave")
+        if (self.frente.colliderect(invasor.misil.rect) or self.alas.colliderect(invasor.misil.rect)) and not self.recuperandose:
+            invasor.misil.rect.y = invasor.rect.y
+            invasor.misil.rect.x = invasor.rect.x+invasor.ancho/2-invasor.misil.ancho/2
+            invasor.misil.disparo = False
+            self.vidas -= 1
+            self.recuperandose = True
 
 
 class Invasor(pygame.sprite.Sprite):
-    def __init__(self, ancho, alto, color):
+    def __init__(self, ancho, alto, color, valor):
         super().__init__()
         self.imagen = pygame.image.load('Parcial_2/img/invasor.png')
         self.ancho = ancho
@@ -60,22 +72,30 @@ class Invasor(pygame.sprite.Sprite):
         self.imagen = pygame.transform.scale(self.imagen, (ancho, alto))
         self.rect = self.imagen.get_rect()
         self.rect.x = random.randrange(0, 600-ancho)
-        self.rect.y = random.randrange(0, 500-alto)
-        self.velocidad_x = random.randrange(1,7)
-        self.velocidad_y = random.randrange(1,7)
+        self.rect.y = random.randrange(-900,-600)
+        self.velocidad_x = random.randrange(1,3)
+        self.velocidad_y = random.randrange(1,3)
         self.color = color
         self.punto_disparo = pygame.Rect(self.rect.x+10, self.rect.y+50-10, 50, 10)
         self.misil = Misil(self.rect.x+self.ancho/2, self.rect.y, 10,30,(0,0,255))
+        self.ingreso = False
+        self.misil.imagen = pygame.transform.rotate(self.misil.imagen, 180)
+        self.velocidad_misil_min= 5
+        self.velocidad_misil_max= 10
+        self.valor = valor
+        self.sonido_disparo = pygame.mixer.Sound("Parcial_2/sound/laser4.wav")
+        self.sonido_disparo.set_volume(0.5)
     
-    def recibir_disparo(self, nave: Nave, puntos: int):
+    def recibir_disparo(self, nave: Nave, puntos: int, lista_invasores):
         if self.punto_disparo.colliderect(nave.misil.rect):
-            self.rect.x = random.randrange(0, 600-self.ancho)
-            self.rect.y = random.randrange(0, 500/2-self.alto)
-            self.punto_disparo = pygame.Rect(self.rect.x+10, self.rect.y+50-10, 50, 10)
+            puntos = puntos + self.valor
+            nave.invasores_destruidos += 1
             nave.misil.rect.y = nave.rect.y
             nave.misil.rect.x = nave.rect.x+nave.ancho/2-nave.misil.ancho/2
             nave.misil.disparo = False
-            puntos = puntos + 1
+            lista_invasores.remove(self)
+            invasor = Invasor(70,50,(0,255,0), 50)
+            lista_invasores.add(invasor)
 
         return puntos
             
@@ -97,9 +117,31 @@ class Invasor(pygame.sprite.Sprite):
             self.velocidad_y += 1
     
     def disparar_misil(self, alto_pantalla):
-        self.misil.disparo = True
-        self.misil.rect.y += random.randrange(3,5)
+        self.misil.rect.y += random.randrange(self.velocidad_misil_min,self.velocidad_misil_max)
         if self.misil.rect.y >= alto_pantalla + alto_pantalla/2:
             self.misil.rect.y = self.rect.y
             self.misil.rect.x = self.rect.x+self.ancho/2-self.misil.ancho/2
             self.misil.disparo = False
+    
+    def ingresar(self):
+        self.rect.y += self.velocidad_y
+        self.punto_disparo = pygame.Rect(self.rect.x+10, self.rect.y+50-10, 50, 10)
+        if not self.misil.disparo:
+            self.misil.rect.y = self.rect.y
+            self.misil.rect.x = self.rect.x+self.ancho/2-5 
+        if self.rect.bottom >= 150:
+            self.ingreso = True
+    
+    def aumentar_dificultad(self, nave):
+        if nave.invasores_destruidos >= 30:
+            self.velocidad_misil_min = 30
+            self.velocidad_misil_max = 40
+            self.valor = 200
+        elif nave.invasores_destruidos >= 20:
+            self.velocidad_misil_min = 20
+            self.velocidad_misil_max = 30
+            self.valor = 100
+        elif nave.invasores_destruidos >= 10:
+            self.velocidad_misil_min = 10
+            self.velocidad_misil_max = 20
+            self.valor = 75
--- parcial2.py
+++ parcial2.py
@@ -1,16 +1,29 @@
 import pygame
+import pygame.mixer
 import random
+import re
 import sys
 import objetos
 
 def crear_invasores(cantidad, lista_invasores):
 
     for _ in range(cantidad):
-        invasor = objetos.Invasor(70,50,(0,255,0))
+        invasor = objetos.Invasor(70,50,(0,255,0), 50)
         lista_invasores.add(invasor)
 
+def temporizador(tiempo: dict):
+    tiempo['segundos'] += 1
+    if tiempo['segundos']>59:
+        tiempo['segundos'] = 0
+        tiempo['minutos'] += 1
+    
+    #segundos_txt = str(tiempo['segundos']).zfill(2)
+    #minutos_txt =  str(tiempo['minutos']).zfill(2)
+
+    return tiempo
 
 pygame.init()
+pygame.mixer.init()
 
 fondo_imagen = pygame.image.load("Parcial_2/img/fondo.png")
 ALTO_PANTALLA = 800
@@ -19,7 +32,8 @@ ANCHO_PANTALLA = 600
 nave = objetos.Nave(350,700,50,50,(255,0,0))
 invasores = pygame.sprite.Group()
 puntaje = 0
-fuente_puntaje = pygame.font.SysFont('Helvetica', 30)
+fuente = pygame.font.SysFont('Helvetica', 30)
+tiempo = {"minutos": 0, "segundos": 0}
 
 crear_invasores(5, invasores)
 
@@ -28,7 +42,13 @@ running = True
 timer_10_milisegundos = pygame.USEREVENT
 pygame.time.set_timer(timer_10_milisegundos, 10)
 
-timer_segundo = pygame.USEREVENT+1
+timer_disparo_invasores = pygame.USEREVENT+1
+pygame.time.set_timer(timer_disparo_invasores, random.randrange(500, 1500))
+
+timer_recuperacion = pygame.USEREVENT+2
+pygame.time.set_timer(timer_recuperacion, 3000)
+
+timer_segundo = pygame.USEREVENT+3
 pygame.time.set_timer(timer_segundo, 1000)
 
 window = pygame.display.set_mode((ANCHO_PANTALLA,ALTO_PANTALLA))
@@ -42,23 +62,54 @@ while running:
         if evento.type == pygame.QUIT:
             running = False
             sys.exit()
-        if evento.type == pygame.MOUSEMOTION:
+        if evento.type == pygame.MOUSEMOTION and not nave.destruida:
             nave.mover_nave(evento.pos[0])
 
-        if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1:
+        if evento.type == pygame.MOUSEBUTTONDOWN and evento.button == 1 and not nave.destruida:
+            if not nave.misil.disparo:
+                nave.sonido_disparo.play()
             nave.misil.disparo = True
             
-        if evento.type == timer_10_milisegundos:
+        if evento.type == timer_10_milisegundos and not nave.destruida:
             nave.disparar_misil()
             for invasor in invasores:
-                invasor.mover(ALTO_PANTALLA, ANCHO_PANTALLA)
-                invasor.disparar_misil(ALTO_PANTALLA)
+                if invasor.ingreso:
+                    invasor.mover(ALTO_PANTALLA, ANCHO_PANTALLA)
+                    if invasor.misil.disparo :
+                        invasor.disparar_misil(ALTO_PANTALLA)
+                else:
+                    invasor.ingresar()
+        
+        if evento.type == timer_recuperacion and nave.recuperandose:
+            nave.recuperandose = False
+        
+        if evento.type == timer_segundo and not nave.destruida:
+            temporizador(tiempo)
+        
+        for invasor in invasores:
+            if (evento.type == timer_disparo_invasores or invasor.rect.x == nave.rect.x) and not nave.destruida and invasor.ingreso and not invasor.misil.disparo:
+                if not invasor.misil.disparo:
+                    invasor.sonido_disparo.play()
+                invasor.misil.disparo = True
+        
+        if nave.vidas == 0:
+            nave.destruida = True
             
         
     for invasor in invasores:
-        puntaje = invasor.recibir_disparo(nave, puntaje)
-        puntaje_render = fuente_puntaje.render(f"Puntaje: {puntaje}", True, (255,255,255))
+        puntaje = invasor.recibir_disparo(nave, puntaje, invasores)
+        puntaje_render = fuente.render(f"Puntaje: {puntaje}", True, (255,255,255))
         nave.recibir_disparo(invasor)
+        invasor.aumentar_dificultad(nave)
+    
+
+    
+    vidas_render = fuente.render(f"Vidas: {nave.vidas}", True, (255,255,255))
+    
+    segundos_txt = str(tiempo['segundos']).zfill(2)
+    minutos_txt =  str(tiempo['minutos']).zfill(2)
+    tiempo_txt = f"{minutos_txt}:{segundos_txt}"
+    tiempo_render = fuente.render(tiempo_txt, True, (255,255,255))
     
     window.fill((0,0,0))
     window.blit(fondo_imagen, (0,0))
@@ -73,6 +124,8 @@ while running:
         window.blit(invasor.misil.imagen, invasor.misil.rect)
         window.blit(invasor.imagen, invasor.rect)
     window.blit(puntaje_render, (5,20))
+    window.blit(vidas_render, (ANCHO_PANTALLA-100,20))
+    window.blit(tiempo_render, (ANCHO_PANTALLA/2-50,20))
     #pygame.draw.rect(window, (0,0,0), nave.frente)
     #pygame.draw.rect(window, (0,0,0), nave.alas)
     pygame.display.flip()
